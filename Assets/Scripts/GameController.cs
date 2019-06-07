f5cca5ff705353779ac9a15eda4de0fa62fab52c
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-06-07 16:55:13 -0300

    Nada
    Primeros pasos

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameController : MonoBehaviour
+{
+    [Range(0f, 0.30f)] // este es un rango para la velocidad 
+    public float parallaxspeed = 0.02f;
+    public RawImage Background;
+    public RawImage Platform;
+    public enum GameState { Idle, Playing }; //parado oo jugando, un enum es una lista de opciones
+    public GameState gamestate = GameState.Idle;
+    public GameObject uiIdle;
+    public GameObject player;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // empezamos el juego
+        // re hacer 2 condicionales If. prepguntamos si estamos quieto, y despues si aprieta una tecla
+        if (gamestate == GameState.Idle)
+        {
+            if (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))
+            {
+                //tecla flecha arriba / o el boton de el mouse posicion 0       {
+                gamestate = GameState.Playing; // el stado de el juego cambio a playing.
+                uiIdle.SetActive(false);
+                player.SendMessage("updateState", "Playerrun");
+                    //llamamos a la el otro script.
+            }
+        }
+        else if (gamestate == GameState.Playing)
+        {
+            parallax();
+        }
+    }
+    void parallax()
+    { //si estamos jugando, empieza el efecto parallax.  el de movimiento
+        float finalspeed;
+        finalspeed = parallaxspeed * Time.deltaTime;
+        //bakcground, en la propiedad uv rect. velocidad en cada eje, x,Y, Altura y ancho
+        Background.uvRect = new Rect(Background.uvRect.x + finalspeed, 0f, 1f, 1f);
+        // ahora para la plataforma 
+        Platform.uvRect = new Rect(Platform.uvRect.x + finalspeed * 3, 0f, 1f, 1f);
+    }
+}
---
+++ Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    private Animator Animacion;
+    void Start()
+    {
+        Animacion = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))
+        {
+            updateState("PlayerJump");
+        }
+    }
+    public void updateState(string state= null)
+    {
+        if (state != null)
+        {
+            Animacion.Play(state);
+        }
+    }
+}
